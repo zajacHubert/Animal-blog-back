0906bd638155509fa2b465ffa68c6d9b48204192
Hubert <hubert.zajac92@gmail.com>
2022-11-01 17:00:16 +0100

    Create register function with hash password

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -1,10 +1,37 @@
 import { Request, Response } from "express";
+import bcrypt from 'bcryptjs';
 import { db } from "../config/db";
 
 
 export const register = async (req: Request, res: Response) => {
-    // const q='SELECT * FROM `users` WHERE `email` = ? OR `username` = ?';
-    // db.query(q)
+    const q = 'SELECT * FROM `users` WHERE `email` = ? OR `username` = ?';
+    db.query(q, [req.body.username, req.body.password], (err, data) => {
+        if (err) {
+            return res
+                .status(500)
+                .json(err)
+        }
+        if (data.length) {
+            return res
+                .status(409)
+                .json('User already exists')
+        }
+
+        const salt = bcrypt.genSaltSync(10);
+        const hash = bcrypt.hashSync(req.body.password, salt);
+
+        const q = 'INSERT INTO users(`username`,`email`,`password`) VALUES (?)'
+        const values = [req.body.username, req.body.email, hash];
+
+        db.query(q, [values], (err, data) => {
+            if (err) {
+                return res.json(err);
+            }
+            return res
+                .status(201)
+                .json('User has been created')
+        });
+    });
 }
 
 export const login = async (req: Request, res: Response) => {

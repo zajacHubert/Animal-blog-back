3b013df4032795bcede4567076b8bb22cc22dd44
Hubert <hubert.zajac92@gmail.com>
2022-11-01 17:07:26 +0100

    Create login function

--- controllers/auth.ts
+++ controllers/auth.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import { v4 as uuid } from 'uuid';
 import { db } from "../config/db";
 
 
@@ -20,8 +22,8 @@ export const register = async (req: Request, res: Response) => {
         const salt = bcrypt.genSaltSync(10);
         const hash = bcrypt.hashSync(req.body.password, salt);
 
-        const q = 'INSERT INTO users(`username`,`email`,`password`) VALUES (?)'
-        const values = [req.body.username, req.body.email, hash];
+        const q = 'INSERT INTO users(`id`,`username`,`email`,`password`) VALUES (?)'
+        const values = [uuid(), req.body.username, req.body.email, hash];
 
         db.query(q, [values], (err, data) => {
             if (err) {
@@ -35,7 +37,35 @@ export const register = async (req: Request, res: Response) => {
 }
 
 export const login = async (req: Request, res: Response) => {
-    console.log('login');
+    const q = 'SELECT * FROM users WHERE username = ?'
+    db.query(q, [req.body.username], (err, data) => {
+        if (err) {
+            return res
+                .status(500)
+                .json(err)
+        }
+        if (data.length === 0) {
+            return res
+                .status(404)
+                .json('User not found')
+        }
+
+        const isPasswordCorrect = bcrypt.compareSync(req.body.password, data[0].password);
+        if (!isPasswordCorrect) {
+            return res
+                .status(400)
+                .json('Wrong passowrd')
+        }
+
+        const token = jwt.sign({ id: data[0].id }, 'jwtkey');
+        const { password, ...other } = data[0];
+        res
+            .cookie('access_token', token, {
+                httpOnly: true,
+            })
+            .status(200)
+            .json(other)
+    })
 }
 
 export const logout = async (req: Request, res: Response) => {

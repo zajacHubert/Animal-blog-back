11923bc0121e36cd6cdf154e89b103b6e287cd19
Hubert <hubert.zajac92@gmail.com>
2022-11-01 17:27:54 +0100

    Create get one post method

--- controllers/post.ts
+++ controllers/post.ts
@@ -16,11 +16,28 @@ export const getPosts = (req: Request, res: Response) => {
         return res
             .status(200)
             .json(data)
-    })
+    });
 }
 
 export const getPost = (req: Request, res: Response) => {
-    console.log('getPost');
+    const q =
+        "SELECT p.id, `username`, `title`, `desc`, `img`, `cat`,`date` FROM users u JOIN posts p ON u.id = p.uid WHERE p.id = ? ";
+
+    db.query(q, [req.params.id], (err, data) => {
+        if (err) {
+            return res
+                .status(500)
+                .json(err)
+        }
+        if (!data.length) {
+            return res
+                .status(404)
+                .json('Post not found')
+        }
+        return res
+            .status(200)
+            .json(data[0])
+    });
 }
 
 export const addPost = (req: Request, res: Response) => {

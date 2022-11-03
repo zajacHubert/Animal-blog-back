8276adc90c953d03357536205724fa9d32c34ae3
Hubert <hubert.zajac92@gmail.com>
2022-11-03 07:33:33 +0100

    Change cors settings

--- index.ts
+++ index.ts
@@ -3,16 +3,17 @@ import express, { json } from 'express';
 import authRouter from './routes/auth';
 import postRouter from './routes/post';
 import fileRouter from './routes/file';
+import cors from 'cors';
 
 const app = express();
 
 app.use(json());
 app.use(cookieParser());
+app.use(cors());
 
 app.use('/auth', authRouter);
 app.use('/post', postRouter);
 app.use('/file', fileRouter);
-
 
 app.listen(3001, () => {
     console.log('Server is running');
